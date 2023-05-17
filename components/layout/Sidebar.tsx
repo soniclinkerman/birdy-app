d961c4aa3852bfb8f065cf2f340cab02c628c19a
Zay <Izpopsonic1@gmail.com>
2023-05-17 10:56:38 -0400

    Implemented geenral responsiveness layout

---
+++ components/layout/Sidebar.tsx
@@ -0,0 +1,60 @@
+import {BsBellFill, BsHouseFill} from "react-icons/bs"
+import {FaUser} from "react-icons/fa"
+import {BiLogOut} from "react-icons/bi"
+import SidebarLogo from "./SidebarLogo"
+import SideBarItem from "./SideBarItem";
+import SidebarTweetButton from "./SidebarTweetButton";
+const Sidebar = () => {
+    const items  = [
+        {
+            label: "Home",
+            href: "/",
+            icon: BsHouseFill,
+        },
+        {
+            label: "Notifications",
+            href: "/notifications",
+            icon: BsBellFill
+        },
+        {
+            label: "Profile",
+            href: "/users/123",
+            icon: FaUser
+        }
+
+    ];
+    return (
+        <div className="col-span-1 h-full pr-4 md:pr-6">
+            <div className="flex flex-col items-end">
+                <div className="space-y-2 lg:w-[230px]">
+                    <SidebarLogo/>
+
+                    {items.map((item) => (
+
+                        <SideBarItem
+                        key={item.href}
+                        label={item.label}
+                        href={item.href}
+                        icon={item.icon}
+                        />
+                    ))}
+
+                    <SideBarItem  
+                    onClick={() => {}}
+                    icon={BiLogOut} 
+                    label="Logout"
+                    />
+
+                    <SidebarTweetButton/>
+      
+
+              
+
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Sidebar
